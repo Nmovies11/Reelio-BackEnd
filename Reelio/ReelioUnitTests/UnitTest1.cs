2e8dec25a1317d58655e3577fa9cf855eabf2ab5
Niels Moone <ps170116@summacollege.nl>
2024-09-25 12:34:20 +0200

    testing CI again true

--- Reelio/ReelioUnitTests/UnitTest1.cs
+++ Reelio/ReelioUnitTests/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace ReelioUnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(false);
+            Assert.IsTrue(true);
         }
     }
 }
