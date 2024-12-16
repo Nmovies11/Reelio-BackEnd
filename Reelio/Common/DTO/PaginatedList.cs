b1be8d89bf521433fd27e7988ecc667da89965ce
Niels <n.moone@student.fontys.nl>
2024-12-16 13:53:00 +0100

    Added paginatedList model to add pagination

---
+++ Reelio/Common/DTO/PaginatedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTO
+{
+    public class PaginatedList<T>
+    {
+        public PaginatedList(List<T> items, int totalCount, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            HasNextPage = pageNumber < TotalPages;
+            HasPreviousPage = pageNumber > 1;
+        }
+
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+    }
+}
